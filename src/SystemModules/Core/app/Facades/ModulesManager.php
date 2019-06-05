19cfbe271a3c0290dde2e95ccc6ae33dd19d08bc
Bastien Chalier <bastien@chalier.io>
2019-06-05 17:25:39 +0200

    fix annotations ModulesManager facade

--- src/SystemModules/Core/app/Facades/ModulesManager.php
+++ src/SystemModules/Core/app/Facades/ModulesManager.php
@@ -6,16 +6,23 @@ use Illuminate\Support\Facades\Facade;
 use SystemModules\Core\App\Models\Module;
 
 /**
- * @method static Module[] getActiveModules()
+ * @method static Module[] getActiveModules($andSystem = false)
  * @method static bool install(string $path, bool $disabled = false)
  * @method static bool uninstall(Module $module)
  * @method static bool delete(Module $module)
  *
- * @inheritDoc \SystemModules\Core\App\Services\ModulesManager
+ * @see \SystemModules\Core\App\Services\ModulesManager
  */
 
 class ModulesManager extends Facade
 {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
     protected static function getFacadeAccessor()
     {
         return \SystemModules\Core\App\Services\ModulesManager::class;
